ef6642f6493dfcbed0c0821962f49ba8446a6351
April Backus <seren.studios@gmail.com>
2025-05-24 22:18:04 -0500

    Light box update for photos and other minor fixes

--- script.js
+++ script.js
@@ -46,3 +46,14 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+function openLightbox(src) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  lightbox.style.display = "flex";
+  lightboxImg.src = src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
